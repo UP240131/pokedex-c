7502301db7c846d6b932c7c96d848010eaee3862
UP240131 <up240131@alumnos.upa.edu.mx>
2026-03-02 15:33:17 -0600

    add getPokemons fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,31 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  //function getPokemons2 () {}
+  useEffect(() => {
+    console.log("entre en pantalla");
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    //parametro
+    try {
+      const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+      const response = await fetch(URL, { method: "GET" });
+
+      if (response.ok) {
+        console.log("respuesta bien");
+      } else {
+        console.log("bard request");
+      }
+    } catch (error) {
+      console.log("ocurrio un error");
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
